689ab7bf2ac293638db01ea3ab03c02c4b8b60ba
hαrsh vαirαgi <harshb7839@gmail.com>
2025-12-01 18:11:06 +0000

    modernized authorization and home pages.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,12 +10,11 @@ function Checkbox({ onChange, checked }: { onChange: (checked: boolean) => void,
     <div className="relative flex justify-center items-center">
       <label htmlFor="checkbox" className="relative flex size-8 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70] p-2 duration-100 hover:p-2.5">
         <input checked={checked} type="checkbox" className="group peer hidden" id="checkbox" onChange={(e) => {onChange(e.currentTarget.checked)}}/>
-        <label htmlFor="checkbox" className="size-full rounded-md bg-background peer-checked:size-0"></label>
-        <div className="absolute left-[0.8rem] h-[4px] w-[14px] -translate-y-10 translate-x-10 rotate-[-41deg] rounded-sm bg-white duration-300 peer-checked:translate-x-0 peer-checked:translate-y-0"
-        ></div>
-        <div className="absolute left-[0.4rem] top-4 h-[4px] w-[10px] -translate-x-10 -translate-y-10 rotate-[45deg] rounded-sm bg-white duration-300 peer-checked:translate-x-0 peer-checked:translate-y-0"></div>
+        <label htmlFor="checkbox" className="size-full rounded-md bg-background peer-checked:size-0"/>
+        <div className="absolute left-[0.8rem] h-[4px] w-[14px] -translate-y-10 translate-x-10 rotate-[-41deg] rounded-sm bg-white duration-300 peer-checked:translate-x-0 peer-checked:translate-y-0"/>
+        <div className="absolute left-[0.4rem] top-4 h-[4px] w-[10px] -translate-x-10 -translate-y-10 rotate-[45deg] rounded-sm bg-white duration-300 peer-checked:translate-x-0 peer-checked:translate-y-0"/>
       </label>
-      <span className="ml-2 flex flex-row w-[1px]">Check Commercials</span>
+      <span className="ml-2 flex flex-row w-[70px] font-bold">Check Commercials</span>
     </div>
   )
 }
@@ -72,13 +71,13 @@ export default function App() {
   }, [])
   return <>
     <div className="max-w-md bg-gradient-to-tr from-background to-transparent">
-    <Logo/>
-      <form onSubmit={handleSearch} className="flex py-10 flex-row w-full justify-center">
+      <Logo/>
+      <form onSubmit={handleSearch} className="flex justify-center py-10 w-full">
         <input
           type="text"
           name="vinnos"
           placeholder="Feed me vin no. column"
-          className="px-4 bg-green-50 text-green-600 placeholder-stone-400 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-contessa focus:ring-2 focus:ring-contessa-dim transition-all cursor-pointer"
+          className="px-4 bg-rose-50 text-[#F25764] placeholder-stone-400 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-[#f25c78] focus:ring-2 focus:ring-[#f79dae] transition-all cursor-pointer"
         />
         <button
           type="submit"
@@ -89,17 +88,17 @@ export default function App() {
         <div className="mx-2 border-l-1"></div>
         <Checkbox checked={service == "service-cv"} onChange={(checked) => {setService(checked ? "service-cv" : "service")}}/>
       </form>
+      {sheet.length > 0 && <div className="mt-2">
+        <div className="max-h-[300px] overflow-auto">
+          <Spreadsheet data={sheet} columnLabels={["VIN", "KYC", "SignUp", "Car Owner"]}/>
+        </div>
+      </div>}
       <div className="w-full flex justify-center py-4 tracking-widest font-mono font-bold">
         {clickables.map(option => <button onClick={option.callback} key={option.title}
           className={`py-2 shadow-lg ${option.color} cursor-pointer mr-2 px-2 h-min text-nowrap rounded-md text-xs`}>
           {option.title}
         </button>)}
       </div>
-      {sheet.length > 0 && <div className="mt-2">
-        <div className="backdrop-brightness-60 max-h-[300px] overflow-auto backdrop-contrast-125 font-mono border-1 border-stone-400 rounded-md p-2">
-          <Spreadsheet data={sheet} columnLabels={["VIN Number", "KYC", "SignUp", "Customer"]}/>
-        </div>
-      </div>}
     </div>
     {notification ? <div className="w-full fixed top-0 flex justify-center">
       <p className="bg-orange-300 text-orange-800 border-3 border-orange-400 px-2 py-0 text-sm font-bold font-mono">
