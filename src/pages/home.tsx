f3d2fe631f76e719887e00a706a52b24f367aa70
hαrsh vαirαgi <harshb7839@gmail.com>
2025-12-01 03:57:27 +0000

    modern ui creation initiated.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,21 @@ import Spreadsheet from "react-spreadsheet";
 import { Loader, Search } from "lucide-react";
 import { SearchConsole } from "@/utils/search";
 
+function Checkbox({ onChange, checked }: { onChange: (checked: boolean) => void, checked: boolean }) {
+  return (
+    <div className="relative flex justify-center items-center">
+      <label htmlFor="checkbox" className="relative flex size-8 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-tr from-[#4158D0] via-[#C850C0] to-[#FFCC70] p-2 duration-100 hover:p-2.5">
+        <input checked={checked} type="checkbox" className="group peer hidden" id="checkbox" onChange={(e) => {onChange(e.currentTarget.checked)}}/>
+        <label htmlFor="checkbox" className="size-full rounded-md bg-background peer-checked:size-0"></label>
+        <div className="absolute left-[0.8rem] h-[4px] w-[14px] -translate-y-10 translate-x-10 rotate-[-41deg] rounded-sm bg-white duration-300 peer-checked:translate-x-0 peer-checked:translate-y-0"
+        ></div>
+        <div className="absolute left-[0.4rem] top-4 h-[4px] w-[10px] -translate-x-10 -translate-y-10 rotate-[45deg] rounded-sm bg-white duration-300 peer-checked:translate-x-0 peer-checked:translate-y-0"></div>
+      </label>
+      <span className="ml-2 flex flex-row w-[1px]">Check Commercials</span>
+    </div>
+  )
+}
+
 export default function App() {
   const { token, removeToken } = useToken();
   const [loading, setLoading] = useState(false);
@@ -23,9 +38,9 @@ export default function App() {
         break;
     }
   }
-  const actionBtns: Array<{ title: string, color: string, callback: () => void }> = [
-    { title: "LOGOUT", color: "bg-rose-500", callback: () => {callbacks('logout')} },
-    { title: "CLEAR", color: "bg-slate-500", callback: () => {callbacks('clear')} }
+  const clickables: Array<{ title: string, color: string, callback: () => void }> = [
+    { title: "LOGOUT", color: "bg-[#D94E67]", callback: () => {callbacks('logout')} },
+    { title: "CLEAR", color: "bg-[#04668C]", callback: () => {callbacks('clear')} }
   ]
   const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -56,43 +71,27 @@ export default function App() {
     })
   }, [])
   return <>
-    <div className="shadow-xl p-4 max-w-md">
-      {sheet.length == 0 && <Logo/>}
-      <form onSubmit={handleSearch} className={"flex flex-row w-full justify-center"}>
+    <div className="max-w-md bg-gradient-to-tr from-background to-transparent">
+    <Logo/>
+      <form onSubmit={handleSearch} className="flex py-10 flex-row w-full justify-center">
         <input
           type="text"
           name="vinnos"
           placeholder="Feed me vin no. column"
-          className="px-4 bg-green-50 text-green-600 placeholder-stone-400 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-green-500 focus:ring-2 focus:ring-green-200 transition-all cursor-pointer"
+          className="px-4 bg-green-50 text-green-600 placeholder-stone-400 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-contessa focus:ring-2 focus:ring-contessa-dim transition-all cursor-pointer"
         />
         <button
           type="submit"
-          className="bg-lime-600 ml-2 p-3 hover:bg-lime-700 text-white rounded-lg font-semibold transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+          className="bg-contessa ml-2 p-3 hover:bg-contessa-dim text-white rounded-lg font-semibold transition-colors disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
         >
           {loading ? <Loader className="animate-spin"/> : <Search/>}
         </button>
+        <div className="mx-2 border-l-1"></div>
+        <Checkbox checked={service == "service-cv"} onChange={(checked) => {setService(checked ? "service-cv" : "service")}}/>
       </form>
-      <div className="mt-4 w-full flex flex-row justify-center">
-        {["personal", "commercial"].map((value, index) => (
-          <span key={value + index} className="mr-3">
-            <input name="hibiscus-cartype"
-              id={"hibiscus-" + value}
-              type="radio"
-              onChange={(e) => {
-                if (e.currentTarget.id == "hibiscus-commercial") setService("service-cv")
-                else setService("service")
-              }}
-              disabled={loading}
-              defaultChecked={index == 0 ? true : false}
-              className="size-4 radio radio-success"
-            />
-            <label className="ml-2 text-lime-400 font-mono uppercase">{value}</label>
-          </span>
-        ))}
-      </div>
-      <div className="w-full flex justify-center mt-2">
-        {actionBtns.map(option => <button onClick={option.callback} key={option.title}
-          className={`py-2 shadow-lg ${option.color} cursor-pointer text-green-100 font-mono mr-2 px-2 h-min text-nowrap`}>
+      <div className="w-full flex justify-center py-4 tracking-widest font-mono font-bold">
+        {clickables.map(option => <button onClick={option.callback} key={option.title}
+          className={`py-2 shadow-lg ${option.color} cursor-pointer mr-2 px-2 h-min text-nowrap rounded-md text-xs`}>
           {option.title}
         </button>)}
       </div>
--- src/pages/suspension.tsx
+++ src/pages/suspension.tsx
@@ -1,15 +1,15 @@
-import { Logo } from "../components";
+import { Logo } from "@/components";
 
 export default function App() {
   return (
-    <div className="py-12 flex flex-col justify-center items-center">
+    <div className="items-center bg-gradient-to-r from-[#191524] to-transparent">
       <Logo/>
-      <div className="p-10 backdrop-brightness-70 backdrop-contrast-125 border-1 border-stone-400 rounded-md">
-        <h1 className="text-2xl font-bold text-lime-400">
-          Waiting for Request
+      <div className="p-10 bg-gradient-to-t from-[#191524] to-transparent to-60%">
+        <h1 className="text-6xl uppercase font-bold">
+          Looking for source
         </h1>
-        <p className="text-lime-100 text-center">
-          Visit KYC portal and search a vin no...
+        <p className="uppercase font-bold text-[12px] tracking-widest">
+          Visit KYC portal and search a vin number
         </p>
       </div>
     </div>
